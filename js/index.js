c847cab18dff70549ee59ce6af6b7c68b29938f9
yves hoebeke <yves.hoebeke@bytesupply.com>
2024-05-20 21:03:15 -0400

    some code cleanup - no functional changes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 window.addEventListener('load', function () {
-  // Event listeners
-  document.getElementById("email-to-clipboard").addEventListener('click', copyToClipboard);
-  document.getElementById("email-to-clipboard").addEventListener('mouseover', (e) => {
-    showClipboardAction(e, true);
+  // Event listeners:
+  //  - email to clipboard.
+  document.getElementById("email-to-clipboard").addEventListener('click', (e) => { 
+    copyToClipboard(e, 'email');
   });
-  document.getElementById("email-to-clipboard").addEventListener('mouseout', (e) => {
-    showClipboardAction(e, false);
+  //  - pgp key to clipboard.
+  document.getElementById("pgp-to-clipboard").addEventListener('click', (e) => { 
+    copyToClipboard(e, 'pgp');
   });
 
+  //  - show intent popup.
+  for (obj of this.document.getElementsByClassName("to-clipboard")){
+    obj.addEventListener('mouseover', (e) => {
+      showClipboardAction(e, true);
+    });
+    obj.addEventListener('mouseout', (e) => {
+      showClipboardAction(e, false);
+    });
+  };
+
+  //  - show work done + intent popup.
   document.getElementById("work-popup").addEventListener('click', () => {
     showWorkContent(true);
   });
@@ -18,16 +30,20 @@ window.addEventListener('load', function () {
     showPopupAction(e, false);
   });  
 
+  //  - close work done by clicking anywhere in the <div>.
   document.getElementById("work-content").addEventListener('click', () => {
     showWorkContent(false);
   });
 
+  // Start day/time clock
   startClock();
+  // Read json to display some data.
   jsonSiteDataInject();
 })
 
+// Clock - tik tok...
 async function startClock() {
-  setInterval(function() {
+  setInterval(() => {
     const time = new Date();
     document.getElementById('doy').innerHTML = time.toDateString();
     document.getElementById('tod').innerHTML = time.toLocaleTimeString("en-US", {hourCycle: 'h23'});
@@ -35,31 +51,75 @@ async function startClock() {
 
 }
 
+// Read json file and insert in appropriate HTML locations
 async function jsonSiteDataInject() {
-  const response = await fetch('https://yveshoebeke.github.io/data/yveshoebeke.github.io.json');
-  const jsonData = await response.json();
-  const time = new Date();
-  
-  // jsonData.worksamples.forEach((obj) => {console.log(obj.company, obj.location,obj.application,obj.technologies)})
-  console.log(jsonData)
-  
-  document.getElementById("email").innerHTML = jsonData.email;
-  document.getElementById("version").innerHTML = jsonData.version;
-  document.getElementById("copy-year").innerHTML = time.getFullYear();
-  document.getElementById("copy-name").innerHTML = jsonData.author;
- }
+  await fetch('data/yveshoebeke.github.io.json')
+  .then((response) => response.json())
+  .then((jsonResponse) => {
+    // email, author, version and (c) year insertion.
+    const time = new Date();
+    document.getElementById("copy-year").innerHTML = time.getFullYear();
+    document.getElementById("email").innerHTML = jsonResponse.email;
+    document.getElementById("version").innerHTML = jsonResponse.version;
+    document.getElementById("copy-name").innerHTML = jsonResponse.author;
 
-function copyToClipboard() {
+    // Work samples <div> content construction.
+    const workUl = document.getElementById("work-content-detail")
+    jsonResponse.worksamples.forEach((obj) => {
+      const companyLi = document.createElement("li");
+      companyLi.classList.add("company");
+      
+      const companyName = document.createTextNode(obj.company + " - " + obj.location);
+      companyLi.appendChild(companyName);
+      workUl.appendChild(companyLi);
+
+        const companyLu = document.createElement("ul");
+
+          const companyApplicationLi = document.createElement("li");
+          const companyApplicationDescription = document.createTextNode(obj.application);
+          companyApplicationLi.appendChild(companyApplicationDescription);
+
+          const companyTechnologiesLi = document.createElement("li")
+          const companyTechnologiesDescription = document.createTextNode(obj.technologies);
+          companyTechnologiesLi.appendChild(companyTechnologiesDescription);
+
+        companyLu.appendChild(companyApplicationLi);
+        companyLu.appendChild(companyTechnologiesLi);
+
+      workUl.appendChild(companyLu);
+    })
+  })
+  .catch((e) => console.error(e));
+}
+
+// Copy content (email or pgp key) to clipboard.
+async function copyToClipboard(e, spanId) {
   showClipboardAction(null, false);
-  content = document.getElementById("email").innerHTML;
-  navigator.clipboard.writeText(content);
 
+  if(spanId == "pgp") {
+    await fetch('data/yves.pub')
+    .then((res) => res.text())
+    .then((text) => {
+      navigator.clipboard.writeText(text);
+    })
+    .catch((e) => console.error(e));
+  } else if(spanId == "email") {
+    content = document.getElementById(spanId).innerHTML;
+    navigator.clipboard.writeText(content);
+  }
+
+  x = e.clientX;
+  y = e.clientY;
+
+  document.getElementById("on-clipboard").style.top = (y-35).toString().concat("px");
+  document.getElementById("on-clipboard").style.left = (x+10).toString().concat("px");
   document.getElementById("on-clipboard").style.visibility = 'visible';
   setTimeout(function() {
     document.getElementById("on-clipboard").style.visibility = 'hidden';
   }, 2000);
 } 
 
+// Clipboard intent popup.
 function showClipboardAction(e, show) {
   if(show) {
     x = e.clientX;
@@ -73,24 +133,30 @@ function showClipboardAction(e, show) {
   }
 }
 
+// Work <div> display control.
 function showWorkContent(show) {
+  var intervalId = 0;
   if(show) {
     showPopupAction(null, false)
     document.getElementById("work-content").style.visibility = 'visible';
-    setTimeout(function() {
+    setTimeout(() => {
       document.getElementById("work-content").style.visibility = 'hidden';
     }, 60000);
 
     document.getElementById("click-to-close").style.visibility = 'visible';
-    setTimeout(function() {
-      document.getElementById("click-to-close").style.visibility = 'hidden';
+    var visibleOrHidden = 'hidden';
+    intervalId = setInterval(() => {
+      visibleOrHidden = (visibleOrHidden == 'hidden')?'visible':'hidden';
+      document.getElementById("click-to-close").style.visibility = visibleOrHidden;
     }, 3000);
   } else {
+    clearInterval(intervalId);
     document.getElementById("click-to-close").style.visibility = 'hidden';
     document.getElementById("work-content").style.visibility = 'hidden';
   }
 }
 
+// Show action 'submitted'.
 function showPopupAction(e, show) {
   if(show) {
     x = e.clientX;
